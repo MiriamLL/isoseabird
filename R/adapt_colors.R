8fc379e83020550d36f57f2c9cf07406c138d794
Miriam Lerma <miriamjlerma@gmail.com>
2023-03-20 16:31:32 +0100

    adapt colors

--- R/adapt_colors.R
+++ R/adapt_colors.R
@@ -11,13 +11,10 @@
 #' @examples SIA_plot_colors<-adapt_colors(SIA_plot=SIA_plot,
 #' color1='#233d4d',color2='#2a9d8f',column_group='Species')
 adapt_colors<-function(SIA_plot=SIA_plot,
-                       column_group=column_group,
                        color1=color1,
                        color2=color2){
 
   SIA_plot_colors<-SIA_plot +
-    ggplot2::geom_point(
-      ggplot2::aes(color = column_group), size = 1.6) +
     ggplot2::scale_color_manual(values = c(color1,color2))
 
 
